4701229d7fe0a8f5d1b306d086e0979b05d237bb
montejagger <hiumathy@gmail.com>
2025-01-15 16:56:52 -0800

    linting error

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -12,7 +12,7 @@ export default AuthService
 async function createAccount(email: string, password: string) {
   try {
     const auth = getAuth(app)
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+    await createUserWithEmailAndPassword(auth, email, password)
     console.log('Successfully Created Account!', email)
     // TODO: Set user cookies - store accessToken in the cookie
   } catch (error: any) {
@@ -25,7 +25,7 @@ async function createAccount(email: string, password: string) {
 async function signIn(email: string, password: string) {
   try {
     const auth = getAuth(app)
-    const userCredential = await signInWithEmailAndPassword(auth, email, password)
+    await signInWithEmailAndPassword(auth, email, password)
     console.log('Successfully Sign In!', email)
     // TODO: Set user cookies - store accessToken in the cookie
   } catch (error: any) {
